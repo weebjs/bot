2bc7048c48ad80992647afecbc60d42612bbd692
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-03-03 17:42:56 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,5 @@
-const figlet = require('figlet');
-
 module.exports = {
-  run: async (client) => {
-    const art = figlet.textSync('yay!');
-
-    console.log('\x1b[34m[INFO]\x1b[0m:', `Logged in as ${client.user.name}`);
-    await client.fetchServers();
-    const status = {
-      content: `Gemini AI • gemini-guilded-chat.vercel.app`,
-      emoteId: 90002555,
-    };
-    await client.setStatus(status);
-    console.log('\x1b[34m[INFO]\x1b[0m:', `${client.servers.cache.size} servers`);
-        console.log(art);
-  },
-};
+  run: (client) => {
+    console.log(`[ ${client.user.name} ] : Connected to Guilded`)
+  }
+}
