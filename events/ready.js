e3d028dfaa72be5e698963dc97418cc436815694
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-03-03 16:59:54 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,11 @@
 module.exports = {
   run: (client) => {
     console.log(`[ ${client.user.name} ] : Connected to Guilded`)
+
+    const status = {
+      content: `Gemini AI • gemini-guilded-chat.vercel.app`,
+      emoteId: 90001800,
+    };
+    await client.setStatus(status);
   }
 }
