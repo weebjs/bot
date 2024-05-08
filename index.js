02f637f8fc15b64b5599c30d14a10fb00f83db44
weebwashere <128880884+weebwashere@users.noreply.github.com>
2024-05-07 21:49:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-const { Client } = require("guilded.js");
-const { Collection } = require("discord.js");
+const { Collection, Client } = require("discord.js");
 const { token, prefix, color, ownerId, mongoURI } = require("./settings.json");
 const functions = require("./handlers/functions");
-const client = new Client({token : token});
-const p = require("primebit.js");
-
+const client = new Client({ token: token });
 const express = require('express');
 const app = express();
 let port = 5000;
 app.get('/', (req, res) => {
   res.send(`Ready`)
-})
-app.listen(port, () => {
-})
+});
+app.listen(port, () => {});
 
-module.exports = {
-  run: async (client) => {
     await client.fetchServers();
-    p.success(`Logged in as ${client.user.name}`);
-    p.log(`Fetched ${client.servers.cache.size} servers!`);
-  },
-};
+    console.log(`Logged in as ${client.user.name}`);
+    console.log(`Fetched ${client.servers.cache.size} servers!`);
 
 client.commands = new Collection();
 client.aliases = new Collection();
 client.settings = { prefix, color, ownerId };
 client.functions = functions;
 
-for(let handler of  ["command", "event"]) require(`./handlers/${handler}`)(client);
+for (let handler of ["command", "event"]) require(`./handlers/${handler}`)(client);
 
 client.login();
