d4dfea1521cd54a58ecd7c51ef234b27cc65b31d
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-05-08 15:43:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-const { Collection, Client } = require("discord.js");
+const { Client } = require("guilded.js");
+const { Collection } = require("discord.js");
 const { token, prefix, color, ownerId, mongoURI } = require("./settings.json");
 const functions = require("./handlers/functions");
-const client = new Client({ token: token });
+const client = new Client({token : token});
+
 const express = require('express');
 const app = express();
 let port = 5000;
 app.get('/', (req, res) => {
   res.send(`Ready`)
-});
-app.listen(port, () => {});
+})
+app.listen(port, () => {
+  console.log(`${port}`)
+})
 
-async function start() {
-  await client.fetchServers();
-  console.log(`Logged in as ${client.user.name}`);
-  console.log(`Fetched ${client.servers.cache.size} servers!`);
+client.commands = new Collection();
+client.aliases = new Collection();
+client.settings = { prefix, color, ownerId };
+client.functions = functions;
 
-  client.commands = new Collection();
-  client.aliases = new Collection();
-  client.settings = { prefix, color, ownerId };
-  client.functions = functions;
+for(let handler of  ["command", "event"]) require(`./handlers/${handler}`)(client);
 
-  for (let handler of ["command", "event"]) require(`./handlers/${handler}`)(client);
-
-  client.login();
-}
-
-start();
+client.login();
