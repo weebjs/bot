f9242ac9c51e43e4c51ba0c8f7470397c8ba120a
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-07-16 03:07:10 +0000

    eh

---
+++ commands/Anime/waifu.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder, Uploader } = require('revolttools.js');
+const axios = require('axios');
+
+exports.default = {
+  name: 'waifu',
+  description: 'generate a random waifu image.',
+  async code(message, args, client) {
+    try {
+      const response = await axios.get('https://api.waifu.im/search?included_tags=waifu');
+
+      const image = response.data.images[0].url;
+
+      const embed = new EmbedBuilder()
+      const uploader = new Uploader(client)
+        .setTitle(':frame_with_picture: Click here to view in HD')
+        .setUrl(image) // Set the URL of the embed to the image URL
+        .setMedia(await uploader.upload(image,"image.png"));
+
+      message.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error(error);
+      let emb = new EmbedBuilder()
+        .setDescription("An error occurred while executing this command.")
+        .setTitle("Error!")
+        .setColour("red");
+
+      message.reply({ embeds: [emb] });
+    }
+  },
+};
